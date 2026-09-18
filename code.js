2a06c7552e56e4d098fdd30a572c0992cc2c443c
gollapudil <gollapudil@vcu.edu>
2026-09-18 15:06:14 -0400

    Update code.js

--- code.js
+++ code.js
@@ -493,7 +493,7 @@ function bookSlot(mentorName, userEmail) {
           "This is an automated message. Please do not reply to this email.";
 
         try {
-          MailApp.sendEmail({ to: studentEmail, subject: emailSubject, body: emailBody });
+          MailApp.sendEmail({  studentEmail, emailSubject, emailBody, {from: "engrstwk2@vcu.edu", name:"Engineering Career Services"} );
         } catch (emailError) {
           console.error("Email sending failed:", emailError); // don't fail the booking if email fails
         }
